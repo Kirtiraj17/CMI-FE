2bac6034332c3f2fcacd4e9365dc6da325d7c767
Kirtiraj 17 <kirtirajg1997@gmail.com>
2024-01-16 17:30:32 +0530

    hero section and Header component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import Header from "./components/Header";
+import "./App.css";
 
 function App() {
   return (
     <>
-     <h1 className='text-red-500'>Hello World</h1>
+      <div className="bg-[url('/hero-img.png')] h-[575px] relative">
+        <Header />
+        <div className="absolute top-[40%] text-white ml-[137px]">
+          <h1 className="text-[64px] font-black leading-none">Latest Styles</h1>
+          <p className="mb-[18px] text-xl font-extrabold">
+            At Yesterday's Prices
+          </p>
+          <button className="font-bold uppercase rounded-xl py-3 px-4 bg-[#CA4022] transition-all duration-300 hover:opacity-85">
+            Browse all styles
+          </button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { CartIcon } from "../icons";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between p-8 pb-12 bg-gradient-to-b from-black to-[rgba(0, 0, 0, 0.0)] text-white fixed w-full">
+      <h1 className="text-lg font-extrabold">
+        <span className="text-[#BFBEBE]">RIGHT</span>FIT.COM
+      </h1>
+      <nav className="w-1/2">
+        <ul className="flex justify-end">
+          <li>
+            <a
+              href="#"
+              className="font-light transition-all hover:font-bold active"
+            >
+              All Products
+            </a>
+          </li>
+          <li className="ml-11">
+            <a href="#" className="font-light transition-all hover:font-bold ">
+              Featured Products
+            </a>
+          </li>
+          <li className="flex ml-11">
+            <CartIcon className="cursor-pointer" />
+            <span className="ml-4">0</span>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
